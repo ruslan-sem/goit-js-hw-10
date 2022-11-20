dc3184eff0813065a6be8516767810bc59ed114c
Ruslan Samoylenko <ruslan.sem@gmail.com>
2022-11-20 16:31:49 +0200

    Init

--- src/index.js
+++ src/index.js
@@ -4,3 +4,10 @@ import Notiflix from 'notiflix';
 import 'notiflix/dist/notiflix-3.2.5.min.css';
 
 const DEBOUNCE_DELAY = 300;
+const input = document.querySelector('#search-box');
+const list = document.querySelector('.country-list');
+const country = document.querySelector('.country-info');
+
+input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+
+function onInput(event) {}
